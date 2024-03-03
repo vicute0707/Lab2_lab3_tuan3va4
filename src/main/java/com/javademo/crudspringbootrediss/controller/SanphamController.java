680651135247b2e6819ffeb2b98a1b190a1b0191
Nguyễn Thị Tường Vi <122417490+vicute0707@users.noreply.github.com>
2024-03-03 20:06:15 +0700

    fisrt comit

--- src/main/java/com/javademo/crudspringbootrediss/controller/SanphamController.java
+++ src/main/java/com/javademo/crudspringbootrediss/controller/SanphamController.java
@@ -1,2 +1,44 @@
-package com.javademo.crudspringbootrediss.controller;public class SanphamController {
+package com.javademo.crudspringbootrediss.controller;
+
+import com.javademo.crudspringbootrediss.enity.Sanpham;
+import com.javademo.crudspringbootrediss.repostory.Sanphamrepo;
+import jakarta.annotation.PostConstruct;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/service")
+
+public class SanphamController {
+    @Autowired
+    Sanphamrepo sanphamrepo;
+    @GetMapping("/getallsanpham")
+    public List<Sanpham> getallSanpham(){
+        List<Sanpham> list = new ArrayList<>();
+        sanphamrepo.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+    @PostConstruct
+    public void saveSP(){
+        Sanpham sanpham = new Sanpham();
+        sanpham.setId(1);
+        sanpham.setName("0 to 1 Vi");
+        sanpham.setIty(1);
+        sanpham.setPrice(1000000000);
+        sanphamrepo.save(sanpham);
+    }
+    @PostMapping("/savesanpham")
+    public Sanpham saveSanpham(@RequestBody Sanpham sanpham){
+        sanphamrepo.save(sanpham);
+        return sanpham;
+
+    }
+    @DeleteMapping("/deletesanpham")
+    public void deleteSanpham(@PathParam("id") Integer id){
+        sanphamrepo.deleteById(id);
+    }
 }
--- src/main/java/com/javademo/crudspringbootrediss/repostory/Sanphamrepo.java
+++ src/main/java/com/javademo/crudspringbootrediss/repostory/Sanphamrepo.java
@@ -1,2 +1,10 @@
-package com.javademo.crudspringbootrediss.repostory;public interface Sanphamrepo {
+package com.javademo.crudspringbootrediss.repostory;
+
+import com.javademo.crudspringbootrediss.enity.Sanpham;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Sanphamrepo extends CrudRepository<Sanpham,Integer> {
+
 }
